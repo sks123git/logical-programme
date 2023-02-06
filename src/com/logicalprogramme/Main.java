c1ea7d250dfe792b0c02892a413f0e7ca94a573d
subzero <shubham8111994@gmail.com>
2023-02-06 20:43:27 +0530

    Added program for perfect number

--- src/com/logicalprogramme/Main.java
+++
@@ -1,7 +0,0 @@
-package com.logicalprogramme;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-    }
-}
---
+++ src/com/logicalprogramme/PerfectNumber.java
@@ -0,0 +1,25 @@
+package com.logicalprogramme;
+import java.util.Scanner;
+
+//Program to print perfect number
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int sum=0;
+        System.out.println("Kindly enter a number to find if its a perfect number");
+        number = scanner.nextInt();
+        for (int i = 1; i < number; i++) {
+            if(number%i==0){                   //checking which is perfectly divisible and taking their sum
+                System.out.print(i + " ");
+                sum+=i;
+            }
+        }
+        System.out.println(" ");
+        if(sum==number){                       //comparing
+            System.out.println("Its a perfect number");
+        }else {
+            System.out.println("Its not a perfect number");
+        }
+    }
+}
