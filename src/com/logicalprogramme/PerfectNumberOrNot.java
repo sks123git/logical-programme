30bd09a008f4689331ccc599e4718f590f3fcdba
subzero <shubham8111994@gmail.com>
2023-02-07 14:30:53 +0530

    updated vaiables in Perfect Number code

--- src/com/logicalprogramme/PerfectNumberOrNot.java
+++ src/com/logicalprogramme/PerfectNumberOrNot.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class PerfectNumberOrNot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num;
+        int number;
         int sum=0;
         System.out.println("Kindly enter a number to find if its a perfect number");
-        num = scanner.nextInt();
-        for (int i = 1; i < num; i++) {
-            if(num%i==0){                   //checking which is perfectly divisible and taking their sum
+        number = scanner.nextInt();
+        for (int i = 1; i < number; i++) {
+            if(number%i==0){                   //checking which is perfectly divisible and taking their sum
                 System.out.print(i + " ");
                 sum+=i;
             }
         }
         System.out.println(" ");
-        if(sum==num){                       //comparing
+        if(sum==number){                       //comparing
             System.out.println("Its a perfect number");
         }else {
             System.out.println("Its not a perfect number");
